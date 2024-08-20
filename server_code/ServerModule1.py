cf5f38bbb5c1a64b5c6b2fed5f8ba105c936103c
jonathan.ries@kingsway.school.nz <jonathan.ries@kingsway.school.nz>
2024-08-20 02:35:06 +0000

    Edited settings, ServerModule1 and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,10 +3,23 @@ from anvil.files import data_files
 import pandas as pd
 import anvil.tables as tables
 from anvil.tables import app_tables
+import xlwings as xw
 
 import anvil.server
 #anvil.server.connect("server_TOW2URT6HPMIOQ4SBOBSXHXW-7GMQAGE3DEN4GXX7")  # Make sure you replace this with your own Uplink ke
+   
+# read by default 1st sheet of an excel file
+#PC_Builder_NZ_data = pd.read_excel('PC_Builder_NZ_data_test.xlsx')
+#print(PC_Builder_NZ_data)
+# Specifying a sheet
 
+ws = xw.Book("Book2.xlsx").sheets['Sheet1']
+
+# Selecting data from
+# a single cell
+v1 = ws.range("A1:A7").value
+
+print("Result:", v1)
 
 '''def import_excel_data(file):
   with open(file, "rb") as f:
