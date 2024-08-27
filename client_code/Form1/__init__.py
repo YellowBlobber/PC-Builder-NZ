eca24e3586a75e9fa818259da37ae1cbacd11b2d
jonathan.ries@kingsway.school.nz <jonathan.ries@kingsway.school.nz>
2024-08-27 02:02:12 +0000

    Edited Form1; Deleted ServerModule1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,15 +1,27 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from anvil.google.drive import app_files
+from random import choice
 import anvil.media
+import anvil.server
+
 
 
 class Form1(Form1Template):
   def __init__(self, **properties):
+    
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    pcs = app_files.pc_builder_nz__google_data
+    print("File is called: " + pcs.title)
+    self.worksheet = pcs[0]
+    print(self.worksheet.fields)
 
     # Any code you write here will run before the form opens.
 
