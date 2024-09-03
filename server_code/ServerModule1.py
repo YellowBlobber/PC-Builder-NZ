5679d9bd909b2f6cb4ef1acf196a32998f96cd29
jonathan.ries@kingsway.school.nz <jonathan.ries@kingsway.school.nz>
2024-09-03 03:03:59 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,30 +1,39 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
 import anvil.files
 from anvil.files import data_files
-import pandas as pd
 import anvil.tables as tables
-from anvil.tables import app_tables
+import anvil.tables.query as q
+import anvil.server
+import anvil.google.drive
 
-import anvil.server  # Make sure you replace this with your own Uplink key
-anvil.server.connect("YOUR-UPLINK-KEY")  # Make sure you replace this with your own Uplink key
+@anvil.server.callable
+def get_sheet_data_cpus():
+  sheet = app_files.pc_builder_nz
+  worksheet = sheet.worksheets[1]
+  return worksheet.rows
 
-def import_csv_data(file):
-  with open(file, "r") as f:
-    df = pd.read_csv(f)
-    for d in df.to_dict(orient="records"):
-      # d is now a dict of {columnname -> value} for this row
-      # We use Python's **kwargs syntax to pass the whole dict as
-      # keyword arguments
-      app_tables.your_table_name_here.add_row(**d)
-import_csv_data("PC Builder NZ data.csv")
-      
-def import_excel_data(file):
-  with open(file, "rb") as f:
-    df = pd.read_excel(f)
-    for d in df.to_dict(orient="records"):
-       #of {columnname -> value} for this row
-       #We use Python's **kwargs syntax to pass the whole dict as
-       #keyword arguments
-      app_tables.table_5.add_row(**d)
-import_excel_data("PC Builder NZ data.csv")
+@anvil.server.callable
+def get_sheet_data_GPU():
+  sheet = app_files.pc_builder_nz
+  worksheet = sheet.worksheets[2]
+  return worksheet.rows
+
+@anvil.server.callable
+def get_unique_categories(sheet_data):
+  categories = set(row['Item Name'] for row in sheet_data)
+  return sorted(list(categories))
 
 
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
