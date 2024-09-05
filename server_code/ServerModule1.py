e20b6bcea7fe71e2619dfd16f0f6f90b40377e6f
jonathan.ries@kingsway.school.nz <jonathan.ries@kingsway.school.nz>
2024-09-05 23:52:15 +0000

    Edited ServerModule1; Deleted Form1.ItemTemplate1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -8,18 +8,86 @@ import anvil.server
 import anvil.google.drive
 
 @anvil.server.callable
+#defining a function that calls the information in the index worksheet 1, in google sheets. [1] being the cpu worksheet.
+#the index [0] worksheet contains all items therefor we do not use index page 0 unless we are wanting all items.
 def get_sheet_data_cpus():
-  sheet = app_files.pc_builder_nz
-  worksheet = sheet.worksheets[1]
-  return worksheet.rows
+  sheet_cpu = app_files.pc_builder_nz
+  worksheet_cpu = sheet_cpu.worksheets[1]
+  return worksheet_cpu.rows
 
 @anvil.server.callable
-def get_sheet_data_GPU():
-  sheet = app_files.pc_builder_nz
-  worksheet = sheet.worksheets[2]
-  return worksheet.rows
+#defining a function that calls the information in the index worksheet 2, in google sheets. [2] being the gpu worksheet.
+def get_sheet_data_gpu():
+  sheet_gpu = app_files.pc_builder_nz
+  worksheet_gpu = sheet_gpu.worksheets[2]
+  return worksheet_gpu.rows
 
 @anvil.server.callable
+#motherbrd stands for Motherboards,
+#defining a fuction that calls the infomation in the index worksheet 3, in google sheets. [3] being the motherboard worksheet.
+def get_sheet_data_motherbrd():
+  sheet_motherbrd = app_files.pc_builder_nz
+  worksheet_motherbrd = sheet_motherbrd.worksheets[3]
+  return worksheet_motherbrd.rows
+  
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_case():
+  sheet_case = app_files.pc_builder_nz
+  worksheet_case = sheet_case.worksheets[4]
+  return worksheet_case.rows
+
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_ram():
+  sheet_ram = app_files.pc_builder_nz
+  worksheet_ram = sheet_ram.worksheets[5]
+  return worksheet_ram.rows
+
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_storage():
+  sheet_storage = app_files.pc_builder_nz
+  worksheet_storage = sheet_storage.worksheets[6]
+  return worksheet_storage.rows
+
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_fans():
+  sheet_fans = app_files.pc_builder_nz
+  worksheet_fans = sheet_fans.worksheets[7]
+  return worksheet_fans.rows
+
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_cpu_cooler():
+  sheet_cpu_cooler = app_files.pc_builder_nz
+  worksheet_cpu_cooler = sheet_cpu_cooler.worksheets[8]
+  return worksheet_cpu_cooler.rows
+
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_adapters():
+  sheet_adapters = app_files.pc_builder_nz
+  worksheet_adapters = sheet_adapters.worksheets[9]
+  return worksheet_adapters.rows
+
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_psu():
+  sheet_psu = app_files.pc_builder_nz
+  worksheet_psu = sheet_psu.worksheets[10]
+  return worksheet_psu.rows
+
+@anvil.server.callable
+#defining a fuction that calls the infomation in the index worksheet 4, in google sheets. [4] being the case worksheet.
+def get_sheet_data_os():
+  sheet_os = app_files.pc_builder_nz
+  worksheet_os = sheet_os.worksheets[11]
+  return worksheet_os.rows
+
+@anvil.server.callable
+#this fuction calls the item names in the picked index for worksheet eg 3, allows us to call and use the names of items in the form code
 def get_unique_categories(sheet_data):
   categories = set(row['Item Name'] for row in sheet_data)
   return sorted(list(categories))
@@ -27,13 +95,12 @@ def get_unique_categories(sheet_data):
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
-#
+
 # To allow anvil.server.call() to call functions here, we mark
 # them with @anvil.server.callable.
 # Here is an example - you can replace it with your own:
-#
+
 # @anvil.server.callable
 # def say_hello(name):
 #   print("Hello, " + name + "!")
 #   return 42
-#
