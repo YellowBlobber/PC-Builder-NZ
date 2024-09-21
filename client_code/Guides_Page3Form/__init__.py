32f2d223053411e5f3d54fc163b4c813fe779467
jonathan.ries@kingsway.school.nz <jonathan.ries@kingsway.school.nz>
2024-09-21 04:42:37 +0000

    Created Guides_Page3Form

---
+++ client_code/Guides_Page3Form/__init__.py
@@ -0,0 +1,24 @@
+from ..Guides_Home import Guides_Home
+from ._anvil_designer import Guides_Page3FormTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from random import choice
+import anvil.media
+import anvil.server
+import anvil.users
+from datetime import datetime
+from anvil import alert
+
+
+class Guides_Page3Form(Guides_Page3FormTemplate):
+  def __init__(self, **properties):
+    self.init_components(**properties)
+
+  def return_button_click(self, **event_args):
+    form = Guides_Home()
+    open_form(form)
