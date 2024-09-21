bf253f78c9815f40b93bbf008099256b1198cd44
cooper.alden@kingsway.school.nz <cooper.alden@kingsway.school.nz>
2024-09-21 23:49:15 +0000

    Edited Guides_Page2Form, Guides_Page1Form and Guides_Page3Form

--- client_code/Guides_Page1Form/__init__.py
+++ client_code/Guides_Page1Form/__init__.py
@@ -23,6 +23,40 @@ class Guides_Page1Form(Guides_Page1FormTemplate):
     form = Guides_Home()
     open_form(form)
 
+  def sign_out_button_click(self, **event_args):
+    anvil.users.logout()
+    alert("You have been signed out.")
+
+  def builder_button_click(self, **event_args):
+    open_form('Form1')
+
+  def login_button_click(self, **event_args):
+    # Show Anvil's built-in login form
+    user = anvil.users.login_with_form()
+    
+    if user:
+        alert(f"Welcome, {user['email']}!")
+        self.login_button.visible = False
+        self.my_account_button.visible = True
+        self.my_account_button.text = (f"{user['email']}")
+    else:
+        alert("Login failed or was canceled.")
+
+  def catalogue_button_click(self, **event_args):
+    if self.catalogue_panel.visible:  # If the label is currently visible
+      self.catalogue_panel.visible = False  # Hide the label
+      self.catalogue_button.icon = "fa:angle-right"
+    else:
+      self.catalogue_panel.visible = True  # Show the label
+      self.catalogue_button.icon = "fa:angle-down"
+      
+  def nav_button_click(self, **event_args):
+ # Toggles the visibility of the label on each button click
+    if self.menu_panel.visible:  # If the label is currently visible
+      self.menu_panel.visible = False  # Hide the label
+    else:
+      self.menu_panel.visible = True  # Show the label
+
   def guides_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass
